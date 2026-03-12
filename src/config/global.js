f8faad78724ea78c657b503439a8516959b42bd8
Cielo_A <cielodamarisangulo@gmail.com>
2026-03-12 07:21:01 -0500

    pdf

--- src/config/global.js
+++ src/config/global.js
@@ -134,7 +134,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/524707_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
